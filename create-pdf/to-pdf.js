11545bd131de137b9d1104b56d30836debf170b1
klequis <klequis@gmail.com>
2019-05-22 14:10:18 -0700

    filterAndAddPath() did not need to be a promise

--- create-pdf/to-pdf.js
+++ create-pdf/to-pdf.js
@@ -3,25 +3,23 @@ const path = require('path')
 const fse = require('fs-extra')
 
 const srcDir = '../manuscript'
-const outDir = 'out'
 
 const filterAndAddPath = (files) => {
-  return new Promise((resolve, reject) => {
-    try {
-      const mdFiles = files
-        .filter(f => path.extname(f) === '.md')
-        .map(f => `${srcDir}/${f}`)
-      resolve(mdFiles)
-    }
-    catch (e) {
-      reject(e)
-    }
-  })
+  try {
+    const mdFiles = files
+      .filter(f => path.extname(f) === '.md')
+      .map(f => `${srcDir}/${f}`)
+    return mdFiles
+  }
+  catch (e) {
+    console.log('filterAndAddPath', e)
+    
+  }
 }
 
 const main4 = async ()  => {
   const allFiles = await fse.readdir(srcDir)
-  const mdFiles = await filterAndAddPath(allFiles)
+  const mdFiles = filterAndAddPath(allFiles)
   const bookPath = 'book.pdf'
   markdownpdf()
     .concat.from(mdFiles)
@@ -31,4 +29,3 @@ const main4 = async ()  => {
 }
 
 main4()
-
